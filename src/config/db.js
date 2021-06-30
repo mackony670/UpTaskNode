78caf15549ccbc5a659b0b8effa742143dc479ab
mackony670 <mackony670@gmail.com>
2021-06-30 01:06:17 -0400

    [config] sqlite3

--- src/config/db.js
+++ src/config/db.js
@@ -5,19 +5,26 @@ const { Sequelize } = require('sequelize');
 
 
 // Option 2: Passing parameters separately (other dialects)
-const db = new Sequelize('uptasknode', 'root', 'root', {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: '3306',
-    define: {
-        timestamps: false
+// const db = new Sequelize('uptasknode', 'root', 'root', {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: '3306',
+//     define: {
+//         timestamps: false
+//     },
+//     ppool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//       }
+// });
+const db = new Sequelize('Uptask', '', '', {
+    dialect: 'sqlite',
+    storage: 'uptask.sqlite', // or ':memory:'
+    define:{
+        underscored:true
     },
-    ppool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-      }
+    operatorsAliases:false
 });
-
 module.exports = db; 
